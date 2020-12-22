ab28b652213334aef6782bbf61243fa1c7c2a465
julia-t07 <62291555+julia-t07@users.noreply.github.com>
2020-12-22 22:50:05 +0200

    Add files via upload

---
+++ hello1.c
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2017, GlobalLogic Ukraine LLC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    This product includes software developed by the GlobalLogic.
+ * 4. Neither the name of the GlobalLogic nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY GLOBALLOGIC UKRAINE LLC `AS IS` AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL GLOBALLOGIC UKRAINE LLC BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/printk.h>
+
+#include <linux/types.h>
+#include <hello1.h>
+
+MODULE_AUTHOR("Trandashyr Julia");
+MODULE_DESCRIPTION("AK Laboratory work #6");
+MODULE_LICENSE("Dual BSD/GPL");
+EXPORT_SYMBOL(print_hello);
+static struct my_list_head *head;
+static int print_hello(uint count)
+{
+int i;
+struct my_list_head *temp_head1;
+struct my_list_head *temp_head2;
+head = kmalloc(sizeof(struct my_list_head*), GFP_KERNEL);
+temp_head1 = head;
+if (count == 0)
+{
+pr_warn("c = 0");
+}
+else if (count >= 5 && count <= 10)
+{
+pr_warn("5 < c < 10");
+}
+else if (count > 10)
+{
+pr_err("c > 10");
+return -EINVAL;
+}
+for (i = 0; i < count; i++)
+{
+temp_head1->next = kmalloc(sizeof(struct my_list_head*), GFP_KERNEL);
+temp_head1->time = ktime_get();
+pr_info("Hello, world\n");
+temp_head1->post_time = ktime_get();
+temp_head2 = temp_head1;
+temp_head1 = temp_head1->next;
+}
+kfree(temp_head2->next);
+temp_head2->next = NULL;
+return 0;
+}
+static int __init hello1_init(void)
+{
+pr_info("Hello1 started");
+pr_info("");
+return 0;
+}
+static void __exit hello1_exit(void)
+{
+struct my_list_head* temp_first = head;
+struct my_list_head* temp_second = temp_first;
+while(temp_first != NULL)
+{
+pr_info("Working time: %lld", temp_first->post_time - temp_first->time);
+temp_second = temp_first;
+temp_first=temp_second->next;
+kfree(temp_second);
+}
+pr_info("Hello1 finished");
+pr_info("");
+}
+module_init(hello1_init);
+module_exit(hello1_exit);
